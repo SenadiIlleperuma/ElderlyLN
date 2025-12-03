e78594d118b20dbbd3c23088aedc294d2e9d5f3f
Senadi Illeperuma <senadi.20220678@iit.ac.lk>
2025-12-03 14:27:12 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express= require('express');
 const db = require('./db');
 const cors = require('cors');
-
+const authRouter= require('./routes/auth.route');
 const app= express();
 const PORT =process.env.PORT || 3000;
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-
+app.use('/app/auth',authRouter)
 //test API
 app.get ('/api/test',async(req, res)=>{
     try{
