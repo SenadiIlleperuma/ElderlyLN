78c7ce3e3ccfae1447e94989908aa67b632eca80
Senadi Illeperuma <senadi.20220678@iit.ac.lk>
2025-12-03 12:53:44 +0530

    create server.js

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express= require('express');
+const db = require('./db');
+const cors = require('cors');
+
+const app= express();
+const PORT =process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+//routes
+
+//test API
+app.get ('/api/test',async(req, res)=>{
+    try{
+        const result= await db.query('SELECT \'Server is running and DB is connected\' AS message;');
+        res.status(200).json(result.rows[0]);
+    }catch (error){
+        res.status(500).send('Server error.Check DB connection logs.');
+    }
+});
+
+//starting the server
+app.listen(PORT, ()=>{
+    console.log(`ElderlyLN Backend running on port ${PORT}`);
+    console.log(`Test API at: http://localhost:${PORT}/api/test\n`);
+});
+
